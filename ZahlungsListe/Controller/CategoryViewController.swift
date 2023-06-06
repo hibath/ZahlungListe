ab1429867f13a3514e50434d745a1e3896cf5a43
hibaagha <Diadora123>
2023-06-06 14:09:19 +0200

    updating category ViewController

--- ZahlungsListe/Controller/CategoryViewController.swift
+++ ZahlungsListe/Controller/CategoryViewController.swift
@@ -3,7 +3,7 @@
 //  ZahlungList
 //
 //  Created by Heba Thabet Agha on 24.01.23.
-//
+//  this controller is not called anymore
 
 import UIKit
 import RealmSwift
@@ -24,11 +24,10 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
     var payingactArray = [PayingAct]()
     var totalPaying = 0
     let realm = try! Realm()
-    let userDefaults = UserDefaults.standard
     let categoryMG = CategoryManager()
-
     
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 80
@@ -38,16 +37,16 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
         getExpenses()
     }
     
-//in order to refrech data when adding new item and going back
+    //in order to refrech data when adding new item then going back
     override func viewWillAppear(_ animated: Bool) {
         tableView.reloadData()
     }
-
+    
     
     func saveData(cat: Category) {
         do {
             try  realm.write {
-                 realm.add(cat)
+                realm.add(cat)
             }
         } catch {
             print("error trying save data")
@@ -59,7 +58,7 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "adding", style: .default) { action1 in
-           //what will happen when the user click the adding button on our alert
+            //what will happen when the user click the adding button on our alert
             let newCat = Category()
             newCat.name = textField.text!
             self.saveData(cat: newCat)
@@ -69,55 +68,18 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
             alertTextField.placeholder = "add new Category here"
             textField = alertTextField
         }
-
+        
         alert.addAction(action)
         alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: {
             action in
-                 // Called when user taps outside
+            // Called when user taps outside
         }))
         present(alert, animated: true, completion: nil)
     }
     
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categoryArray?.count ?? 1
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CaregoryCell
-        
-        cell.delegate = self
-        if let cat = categoryArray?[indexPath.row]{
-            let p = getPaying(category: cat)
-            cell.name.text = cat.name
-            cell.value.text = String(p)+" $"
-        }
-        return cell
-    }
     
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        guard orientation == .right else { return nil }
-
-        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-            // handle action by updating model with deletion
-            self.updateModel(at: indexPath)
-            print("deleted")
-          //  tableView.reloadData()
-        }
-
-        // customize the action appearance
-        deleteAction.image = UIImage(named: "delete-icon")
-
-        return [deleteAction]
-    }
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
-        var options = SwipeOptions()
-        options.expansionStyle = .destructive
-        options.transitionStyle = .border
-        return options
-    }
-    
-     func updateModel(at indexPath: IndexPath) {
+    func updateModel(at indexPath: IndexPath) {
         do {
             try self.realm.write {
                 self.realm.delete(self.categoryArray![indexPath.row])
@@ -127,9 +89,7 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
         }
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "goToPayingAct", sender: self)
-    }
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! PayingActTableViewController
@@ -137,13 +97,13 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
             destinationVC.selectedCategory = categoryArray?[indexPath.row]
         }
     }
-
+    
     func getPaying(category: Category) -> Int {
         var sum : Int = 0
         let payingArray = category.payings
-                for i in payingArray{
-                    sum = sum + Int(i.value)
-                }
+        for i in payingArray{
+            sum = sum + Int(i.value)
+        }
         return sum
     }
     
@@ -167,8 +127,6 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
         return expensesDic
     }
     
-    
-    
     @IBAction func setBudgetPressed(_ sender: UIButton) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Set Budget", message: "", preferredStyle: .alert)
@@ -177,7 +135,7 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
             bud.value  = Int(textField.text!)!
             do {
                 try  self.realm.write {
-                     self.realm.add(bud)
+                    self.realm.add(bud)
                 }
             } catch {
                 print("error trying save data")
@@ -191,13 +149,58 @@ class CategoryViewController: UITableViewController, SwipeTableViewCellDelegate{
         alert.addAction(action)
         alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: {
             action in
-                 // Called when user taps outside
+            // Called when user taps outside
         }))
         present(alert, animated: true, completion: nil)
         
     }
     
     
+    // MARK: - table Delegate, Datasource
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray?.count ?? 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CaregoryCell
+        
+        cell.delegate = self
+        if let cat = categoryArray?[indexPath.row]{
+            let p = getPaying(category: cat)
+            cell.name.text = cat.name
+            cell.value.text = String(p)+" $"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        options.transitionStyle = .border
+        return options
+    }
+    
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            self.updateModel(at: indexPath)
+            print("deleted")
+            //  tableView.reloadData()
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToPayingAct", sender: self)
+    }
 }
-
